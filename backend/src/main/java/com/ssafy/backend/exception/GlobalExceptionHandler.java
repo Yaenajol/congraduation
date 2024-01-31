73140502f22a90a669b126b562da1c880d7c2b85
tjwjdgus83 <tjwjdgus83@naver.com>
2024-02-01 00:28:00 +0900

    tjwjdgus83 docs : modify internel error message

--- backend/src/main/java/com/ssafy/backend/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/ssafy/backend/exception/GlobalExceptionHandler.java
@@ -8,10 +8,9 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
   @ExceptionHandler(Exception.class)
-  public ResponseEntity<Exception> handleException(Exception e) {
+  public ResponseEntity<Object> handleException(Exception e) {
     e.printStackTrace();
-
-    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e);
+    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("예기치 않은 내부 오류가 발생했습니다");
   }
     @ExceptionHandler(CustomException.class)
     public ResponseEntity<Object> handleUserNotFoundException(CustomException ce){
